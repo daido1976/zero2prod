1e25a91192ae67a9b5f3095204bb1612f18e1ab2
daido1976 <daido1976@gmail.com>
2022-08-13 17:15:04 +0900

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{app, config};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // NOTE: Logging is controlled via the RUST_LOG environment variable.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let config = config::get_configuration();
     let connection = PgPool::connect(&config.database_url)
         .await
